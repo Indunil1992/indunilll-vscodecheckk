389ed8e7bcc1f610872af9e1fab2f4df936fae9c
Indunil1992 <indunil@adroitlogic.com>
2021-02-01 14:26:15 +0530

    com1

---
+++ indunilll-vscodecheckk.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+    debugger;
+    try {
+        let data = await s3.listObjects({
+            Bucket: "induuuu",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
+};
